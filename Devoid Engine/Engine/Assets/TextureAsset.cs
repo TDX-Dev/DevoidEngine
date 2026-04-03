4aec612f24e3b490739c7ef0ee4794570113f5cb
MohamedAshiqRafeeqAhmed <142332745+TDX-Dev@users.noreply.github.com>
2026-04-04 02:52:28 +0530

    New

---
+++ Devoid Engine/Engine/Assets/TextureAsset.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevoidEngine.Engine.Assets
+{
+    [MessagePackObject]
+    public class TextureAsset
+    {
+        [Key(0)]
+        public int Width;
+
+        [Key(1)]
+        public int Height;
+
+        [Key(2)]
+        public TextureFormat Format;
+
+        [Key(3)]
+        public int MipCount;
+
+        [Key(4)]
+        public byte[] PixelData = Array.Empty<byte>();
+    }
+}
