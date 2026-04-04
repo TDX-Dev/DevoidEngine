628bb207006a9ceeff8abd2b4f7fa54f13bef892
MohamedAshiqRafeeqAhmed <142332745+TDX-Dev@users.noreply.github.com>
2026-04-04 13:34:57 +0530

    Serializaton

---
+++ Devoid Engine/Engine/Assets/SceneAsset.cs
@@ -0,0 +1,20 @@
+﻿using DevoidEngine.Engine.Core;
+using MessagePack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevoidEngine.Engine.Assets
+{
+    [MessagePackObject]
+    public class SceneAsset
+    {
+        [Key(0)]
+        public string Name = "";
+        [Key(1)]
+        public List<GameObject> GameObjects = new List<GameObject>();
+
+    }
+}
