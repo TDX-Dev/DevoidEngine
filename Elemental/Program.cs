120d4f6a9405d7f10ca53f293010837fa6833952
MohamedAshiqRafeeqAhmed <142332745+TDX-Dev@users.noreply.github.com>
2026-09-09 23:26:50 +0530

    Elemental 2

--- Elemental/Program.cs
+++ Elemental/Program.cs
@@ -1,28 +1,10 @@
-﻿using DevoidEngine.Core;
-
-namespace Elemental
+﻿namespace Elemental
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            var application = new Application(new ApplicationSpecification()
-            {
-                Height = 720,
-                Width = 1280,
-                Name = "Elemental Editor",
-                API = GraphicsAPI.DX11,
-                Resizable = true,
-                VSync = false
-            });
-
-            application.AddLayer(new EditorLayer());
-
-            Engine.Instance.ProjectSystem.Load(
-                "D:/Devoid Engine/DevoidProject/new_project.devoid"
-            );
-
-            application.Run();
+            Console.WriteLine("Hello, World!");
         }
     }
 }
